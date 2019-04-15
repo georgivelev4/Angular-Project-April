b1358f8114164bef4f5306881e201f781ec9abe6
Georgi Velev <georgivelev4@gmail.com>
2019-04-15 20:15:39 +0300

    Final updates on the server
    Fetching data onInit was causing error in anonymous-home part

--- server/routes/feed.js
+++ server/routes/feed.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const feedController = require('../controllers/feed');
 const isAuth = require('../middleware/is-auth');
 
-router.get('/courses', isAuth,feedController.getCourses);
+router.get('/courses',feedController.getCourses);
 router.post('/course/create',isAuth, feedController.createCourse);
 router.post('/takecourse/:id', isAuth, feedController.takeCourse);
 router.post('/mycourses', isAuth, feedController.myCourses);
